71ac852f5573700090e32ca8d22e98599627ea53
junjie-w <junjiewu.brooke@gmail.com>
2024-11-16 21:18:00 +0100

    chore(release): add changelog generation

--- .releaserc.js
+++ .releaserc.js
@@ -3,10 +3,11 @@ export default {
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
+    '@semantic-release/changelog',
     '@semantic-release/npm',
     '@semantic-release/github',
     ['@semantic-release/git', {
-      assets: ['package.json', 'package-lock.json'],
+      assets: ['package.json', 'package-lock.json', 'CHANGELOG.md'],
       message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
     }]
   ]
